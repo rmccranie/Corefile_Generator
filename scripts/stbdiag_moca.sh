9def4c0cda5a7fd2ae0ba9be6f7a7deebdd09d77
Rodney McCranie <rmccranie@centos-machine.(none)>
2015-06-16 04:57:51 -0400

    Refactoring: add date to filename, Improve cleanup.

--- scripts/stbdiag_moca.sh
+++ scripts/stbdiag_moca.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+DATE=$( date +%Y%m%d_%H%M%S )
+
+do_cleanup() {
+    pcap_filename=$1
+    rm -rf /mnt/persist/stbdiag_*.sh
+    rm -rf $pcap_filename
+}
 
 echo "Downloading utilities."
 
 success=`curl -L https://www.dropbox.com/s/ldmmn0dcr058iod/stbdiag_utils.sh?dl=0 -o /mnt/persist/stbdiag_utils.sh `
-chmod +x /mnt/persist/stbdiag.sh
 source /mnt/persist/stbdiag_utils.sh
 
 if [ -e /mnt/persist/stbdiag_utils.sh ]; then
@@ -14,10 +20,10 @@ else
 fi
 
 echo "Starting tcpdump in the background."
-tcpdump -i br0 -vv -s 0 -l host 255.255.255.251 or host 239.255.255.251 or port 2100 or port 19798 -w /mnt/persist/Minerva-$HOSTNAME.pcap > /dev/null 2>&1 &
+tcpdump -i br0 -vv -s 0 -l host 255.255.255.251 or host 239.255.255.251 or port 2100 or port 19798 -w /mnt/persist/Minerva-$HOSTNAME-$DATE.pcap > /dev/null 2>&1 &
 
 while true; do
-  read -p "Press any key when ready to stop tcpdump and proceed" yn
+  read -p "Press any key when ready to stop tcpdump and proceed: " yn
   case $yn in
        * ) break;;
   esac
@@ -25,6 +31,14 @@ done
 
 killall tcpdump
 
-upload_to_dropbox dumps /mnt/persist/Minerva-$HOSTNAME.pcap
+upload_to_dropbox dumps /mnt/persist/Minerva-$HOSTNAME-$DATE.pcap
 
+#
+# Cleanup
+#
+read -p "Do you wish to clean up pcap files? [Y/n]" yn
+case $yn in
+ [Nn]* ) echo "Cleanup not performed."; break;;
+     * ) echo "Cleaning up..."; do_cleanup /mnt/persist/Minerva-$HOSTNAME-$DATE.pcap ; break;;
+esac
 
--- scripts/stbdiag_utils.sh
+++ scripts/stbdiag_utils.sh
@@ -1,7 +1,7 @@
 upload_to_dropbox() {
     upload_path=$1
     local_fully_qualified_filename=$2
-    curl -H "Authorization: Bearer zf_DdnPHb5AAAAAAAAAADT9lSd93RJy3HeBShWgENMgso_IYW9Cu48XN6E4PCg15" https://api-content.dropbox.com/1/files_put/auto/$upload_path/ -T $local_fully_qualified_filename 
+    curl -H "Authorization: Bearer zf_DdnPHb5AAAAAAAAAADT9lSd93RJy3HeBShWgENMgso_IYW9Cu48XN6E4PCg15" https://api-content.dropbox.com/1/files_put/auto/$upload_path/ -T $local_fully_qualified_filename >> /dev/null 2>&1
     echo "File $fully_qualified_filename uploaded to QA Dropbox Account."
 }
 
