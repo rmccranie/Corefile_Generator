42dcc16b718520d9ad9b29ceb8dab9d1736192fc
Rodney McCranie <rmccranie@centos-machine.(none)>
2015-06-27 00:36:26 -0400

    Refactor, move functions to separate utils script. Add indication of think status at start and 1 minute of logread per Entone request.

--- scripts/stbdiag.sh
+++ scripts/stbdiag.sh
@@ -2,7 +2,7 @@
 #set -x
 ###############################################################################################################
 #                                                                                                             #
-# This script assumes it is run on a device with /mnt/persist as a persistent directory (should work for      #
+# This script assumes it is run on a device with $LOCAL_SCRIPT_DIR as a persistent directory (should work for      #
 # all Entone versions). It will gather STB diag info and cause a core dump for the THINK client. Following    #
 # this it will upload the tar/gz output to Dropbox. (Tom Simpson)                                             #
 # This script will look for THINK client and cause a coredump.                                                #
@@ -10,149 +10,66 @@
 # from over writing this in some dumb fashion.                                                                #
 #                                                                                                             #
 ###############################################################################################################
-
-CORE_DIR=`cat /proc/sys/kernel/core_pattern | awk 'BEGIN{FS="%"}{print $1}'`
+HOST=$(hostname)
+DATE=$( date +%Y%m%d_%H%M%S )
+LOCAL_SCRIPT_DIR="/mnt/hdd"
+UTILS_SUCCESS=0
 
 clear
+mkdir -p $LOCAL_SCRIPT_DIR/${HOST}/logfiles
+echo "Hello ${HOST}, we are going to run a few things. You will most likely need to reboot afterwards" 
+
+echo -n "Downloading utilities: "
+curl -L https://www.dropbox.com/s/ldmmn0dcr058iod/stbdiag_utils.sh?dl=0 -o $LOCAL_SCRIPT_DIR/stbdiag_utils.sh > /dev/null 2>&1 
+source $LOCAL_SCRIPT_DIR/stbdiag_utils.sh
+
+if [ -e $LOCAL_SCRIPT_DIR/stbdiag_utils.sh ] && [ $UTILS_SUCCESS == 1 ]; then
+    echo "SUCCESS"
+else
+    echo "FAILED!"
+    exit ;
+fi
+
+#
+# Do timed logread command (Entone)
+#
+echo "Doing timed logread (Entone request).  This will take 1 minute."
+do_timed_logread 1 $LOCAL_SCRIPT_DIR/${HOST}/logfiles/logread.out
+
 #
 # Looking for Think client, if it's not running and no core files were left around, EXIT. If it is running, kill it for core file.
 #
+echo -n "Think status at script start: " | tee $LOCAL_SCRIPT_DIR/${HOST}/logfiles/think_status.out
 case "$(pidof think | wc -w)" in
 
-0)  echo "Think Client NOT running. "  ;
+0)  echo -n "Think Client NOT running. " | tee -a $LOCAL_SCRIPT_DIR/${HOST}/logfiles/think_status.out ;
     if [ "$(ls -A $CORE_DIR)" ]; then
-        echo "Core files exist, continuing!" ;
+        echo "Core files exist, continuing!" | tee -a $LOCAL_SCRIPT_DIR/${HOST}/logfiles/think_status.out ;
     else
-        echo "No core files exist. EXITING!" ;
+        echo "No core files exist. EXITING!" | tee -a $LOCAL_SCRIPT_DIR/${HOST}/logfiles/think_status.out ;
         exit ;
     fi
     ;;
-*)  echo "Think Client IS running, killing for core file: $(date)" ;
+*)  echo "Think Client IS running, killing for core file: $DATE" | tee -a $LOCAL_SCRIPT_DIR/${HOST}/logfiles/think_status.out;
     killall -11 think ;
     ;;
 esac
 
-echo "Hello ${h1}, we are going to run a few things. You will most likely need to reboot afterwards" 
-
-h1=$(hostname)
-date=$( date +%Y%m%d_%H%M%S )
-
-mkdir -p /mnt/persist/${h1}/logfiles
-
 echo "Dumping dmesg to file"
-dmesg > /mnt/persist/${h1}/logfiles/dmesg-${date}.out
+dmesg > $LOCAL_SCRIPT_DIR/${HOST}/logfiles/dmesg-${DATE}.out
 echo "Dumping top to file"
-top -b -n1 > /mnt/persist/${h1}/logfiles/top-${date}.out
+top -b -n1 > $LOCAL_SCRIPT_DIR/${HOST}/logfiles/top-${DATE}.out
 
-echo "Gathering diagnostic info"
-hwblk_field() { echo "$status" | sed -ne "/$1/ s|.*: ||p" ; }
+echo "Gathering general STB info"
 
-model_name() {
-	case "$(hwblk_field 'HW Model')" in
-		18) echo "Kamai 400";;
-		19) echo "Kamai 400";;
-		20) echo "Amulet 400";;
-		21) echo "Amulet 400";;
-		22) echo "Amulet 400";;
-		23) echo "Amulet 400";;
-		24) echo "Magi 450";;
-		25) echo "Kamai 400v2";;
-		26) echo "Kamai 500";;
-		27) echo "Amulet 500";;
-		28) echo "Magi 450a";;
-		29) echo "Magi 400";;
-		30) echo "Magi 410";;
-		31) echo "Kamai 450";;
-		32) echo "Amulet 450m";;
-		33) echo "Kamai X";;
-		34) echo "Aria 500c";;
-		35) echo "Aria 500i";;
-		36) echo "Aria 500t";;
-		37) echo "XTV125H-C";;
-		38) echo "XTV125H-2C";;
-		39) echo "Magi 550";;
-		40) echo "Aria 500i";;
-		41) echo "Amulet 600";;
-		42) echo "Kamai 600";;
-		*)  echo "Entone STB";;
-	esac
-}
+print_json > $LOCAL_SCRIPT_DIR/${HOST}/logfiles/generalstbinfo.out
 
-toKilo() { local n; read n; expr $n \/ 1024; }
-toMega() { local n; read n; expr $n \/ 1048576; }
-roundoff() { local x ; read x ; local n="`expr $1 / 2`" ; expr \( "$x" + "$n" \) \/ "$1" \* "$1"; }
-
-sw_version() { sed -ne "/$1/ s|.*: ||p" /tmp/diag/general/status; }
-
-get_uptime() { uptime | sed -e 's|.*up[ \t]*\([^,]\+\),.*|\1|'; }
-
-_df() { df -m | grep $1 | tail -n1 | awk "{print \$$2}"; }
-_df_unit() { local size="$(_df $*)"; if [ "$size" -gt 1024 ]; then echo "$(echo ${size} | toKilo)GB"; elif [ -n "$size" ]; then echo "${size}MB"; else echo "--"; fi ; }
-disk_size() { _df_unit $1 2; }
-disk_used() { _df_unit $1 3; }
-
-nand_size() { grep "nand0." /proc/mtd | awk '{printf "0x"$2 "\n"}' | xargs printf "%d\n" | awk '{s+=$1} END {print s / 1048576}' | roundoff 64; }
-nand_used() { expr $(nand_size) - $(_df `partition_info approotfs` 4) - $(_df /mnt/persist 4); }
-
-ram_system_total() { grep "MemTotal:" /proc/meminfo | awk '{printf $2 }'; }
-ram_system_free() { grep "MemFree:" /proc/meminfo | awk '{printf $2 }'; }
-ram_nexus_total() { heapDump 2>&1 | sed 's,.*size \([0-9]*\).*,\1,g' | awk '{s+=$1} END {print s}' | toKilo; }
-ram_nexus_free() { heapDump 2>&1 | sed 's,.*free \([0-9]*\).*,\1,g' | awk '{s+=$1} END {print s}' | toKilo; }
-ram_size() { grep "System RAM" /proc/iomem | sed 's,\([0-9a-fA-F]\{8\}\)-\([0-9a-fA-F]\{8\}\).*,-0x\1 0x\2 1,g' | xargs printf "%d\n" | awk '{s+=$1} END {print s / 1024}'; }
-ram_used() { expr $(ram_size) - $(ram_system_free) - $(ram_nexus_free); }
-
-JSON_SEPARATOR=""
-_print_value() { printf "$JSON_SEPARATOR\"$1\": \"%s\"" "$2"; JSON_SEPARATOR=", "; }
-
-#netflix_esn() { netflixESN > /dev/null; NETFLIX_ESN=$(cat /tmp/netflix-esn); }
-
-print_json() {
-	printf "{"
-	_print_value 'Model Name' "$(model_name)"
-	_print_value 'Board Version' "$(hwblk_field 'Board Version')"
-	_print_value 'Serial Number' "$(hwblk_field 'HW Model')-$(hwblk_field 'ESN')"
-	_print_value 'PBL Version' "$(sw_version pbl)"
-	_print_value 'BBL Version' "$(sw_version bbl)"
-	_print_value 'APP Version' "$(sw_version app)"
-#	netflix_esn
-#	if [ ! -z ${NETFLIX_ESN} ]; then
-#	    _print_value 'Netflix ESN' "${NETFLIX_ESN}"
-#	fi
-	_print_value 'System Uptime' "$(get_uptime)"
-	_print_value 'Flash Size (Used)' "$(nand_size)MB ($(nand_used)MB)"
-	_print_value 'Ram Size (Used)' "$(ram_size | toKilo)MB ($(ram_used | toKilo)MB)"
-	_print_value 'System Memory' "$(ram_system_free | toKilo) / $(ram_system_total | toKilo)"
-	_print_value 'Nexus Memory' "$(ram_nexus_free | toKilo) / $(ram_nexus_total | toKilo)"
-	_print_value 'persist Size (Used)' "$(disk_size /mnt/persist) ($(disk_used /mnt/persist))"
-	_print_value 'IP Address' "$(hwblk_field 'IP')"
-
-	# TODO: implement error code field
-	#_print_value 'Error Code' '--'
-	printf "}"
-}
-
-upload_to_dropbox() {
-
-    curl -H "Authorization: Bearer zf_DdnPHb5AAAAAAAAAADT9lSd93RJy3HeBShWgENMgso_IYW9Cu48XN6E4PCg15" https://api-content.dropbox.com/1/files_put/auto/uploads/ -T /mnt/persist/${h1}-${date}-core.tar.gz
-    curl -H "Authorization: Bearer zf_DdnPHb5AAAAAAAAAADT9lSd93RJy3HeBShWgENMgso_IYW9Cu48XN6E4PCg15" https://api-content.dropbox.com/1/files_put/auto/uploads/ -T /mnt/persist/${h1}-${date}-logs.tar.gz
-    echo "Files uploaded to QA Dropbox Account."
-    echo ""
-}
-
-do_cleanup() {
-
-    rm -rf /mnt/persist/${h1}*
-    rm -rf /mnt/persist/stbdiag.sh
-}
-
-print_json >/mnt/persist/${h1}/logfiles/generalstbinfo.out
-
-echo "Creating core file archive /mnt/persist/${h1}-${date}-core.tar.gz, this will take a moment."
+echo "Creating core file archive $LOCAL_SCRIPT_DIR/${HOST}-${DATE}-core.tar.gz, this will take a moment."
 
 found=0
 x=1
 
-echo -n "Waiting for core file: ." 
+echo -n "Waiting for core file: #" 
 
 for x in 1 2 3 4 5 6 7 8 9 10
 do
@@ -161,7 +78,7 @@ do
     break
   else
     #echo "$CORE_DIR is Empty"
-    echo -n "."
+    echo -n "#"
     sleep 1
   fi
 done
@@ -173,12 +90,12 @@ if [ $found -eq 0 ]; then
 fi
     
 ls -la $CORE_DIR
-tar -czvf /mnt/persist/${h1}-${date}-core.tar.gz $CORE_DIR/*
-ls -l /mnt/persist/*.gz 
+tar -czvf $LOCAL_SCRIPT_DIR/${HOST}-${DATE}-core.tar.gz $CORE_DIR/*
+ls -l $LOCAL_SCRIPT_DIR/*.gz 
 
-printf "Creating log file archive if available /mnt/persist/${h1}-${date}-logs.tar.gz"
-tar -czvf /mnt/persist/${h1}-${date}-logs.tar.gz /mnt/persist/${h1}/logfiles/*
-ls -l /mnt/persist/*.gz
+printf "Creating log file archive if available $LOCAL_SCRIPT_DIR/${HOST}-${DATE}-logs.tar.gz"
+tar -czvf $LOCAL_SCRIPT_DIR/${HOST}-${DATE}-logs.tar.gz $LOCAL_SCRIPT_DIR/${HOST}/logfiles/*
+ls -l $LOCAL_SCRIPT_DIR/*.gz
 
 # 
 # Check whether upload is desired.
@@ -187,7 +104,8 @@ read -p "Do you wish to upload tar files to Dropbox? [Y/n]" yn
 case $yn in
   [Nn]* ) echo "Upload not performed."; break;;
       * ) echo "Uploading..."; 
-          upload_to_dropbox ;
+          upload_to_dropbox uploads $LOCAL_SCRIPT_DIR/${HOST}-${DATE}-core.tar.gz;
+          upload_to_dropbox uploads $LOCAL_SCRIPT_DIR/${HOST}-${DATE}-logs.tar.gz;
           break;;
 esac
 
@@ -197,7 +115,7 @@ esac
 read -p "Do you wish to clean up core/log files? [Y/n]" yn
 case $yn in
  [Nn]* ) echo "Cleanup not performed."; break;;
-     * ) echo "Cleaning up..."; do_cleanup ; break;;
+     * ) echo "Cleaning up..."; do_cleanup $LOCAL_SCRIPT_DIR/${HOST}*  $LOCAL_SCRIPT_DIR/stbdiag*.sh ; break;;
 esac
 
 #
--- scripts/stbdiag_utils.sh
+++ scripts/stbdiag_utils.sh
@@ -1,8 +1,145 @@
+#!/bin/sh
+CORE_DIR=`cat /proc/sys/kernel/core_pattern | awk 'BEGIN{FS="%"}{print $1}'`
+UTILS_SUCCESS=1
+
 upload_to_dropbox() {
     upload_path=$1
     local_fully_qualified_filename=$2
     curl -H "Authorization: Bearer zf_DdnPHb5AAAAAAAAAADT9lSd93RJy3HeBShWgENMgso_IYW9Cu48XN6E4PCg15" https://api-content.dropbox.com/1/files_put/auto/$upload_path/ -T $local_fully_qualified_filename >> /dev/null 2>&1
     echo "File $fully_qualified_filename uploaded to QA Dropbox Account."
+}
+
+do_cleanup() {
+    local num_args=$#
+    ind=$((num_args))
+    echo $ind
+    while [ 0 -lt $ind ]; do  
+        rm -rf ${@:$ind}
+        ind=$(( ind - 1 ))
+    done
+}
+
+do_timed_logread() {
+    time=$1
+    outfile=$2
+    if [ $# -ne 2 ]; then 
+        echo "illegal number of parameters"
+        return;
+    fi
+
+    if [ -z "$time" ]; then 
+        local time_sec=60; 
+    else
+        local time_sec=$(( time*60 ))
+    fi
+
+    outfile=$2
+    
+    echo -ne "Performing logread: "
+    logread -f > $outfile 2>&1 &
+    # Count down while logread is active
+    local loop_count=0
+    while [ $time_sec -ne 0 ]; do 
+        sleep 1;
+        echo -ne "#"
+        time_sec=$(( time_sec - 1 )) 
+        mod=$(( loop_count % 10 ))
+        if [ $mod -eq 0 ] && [ $loop_count -ne 0 ]; then
+           echo -ne "\033[2K"
+           echo -ne "\rPerforming logread: "
+        fi
+        loop_count=$(( loop_count + 1 ))
+    done
+    
+    echo
+
+    killall logread 
+}
+
+hwblk_field() { echo "$status" | sed -ne "/$1/ s|.*: ||p" ; }
+
+model_name() {
+	case "$(hwblk_field 'HW Model')" in
+		18) echo "Kamai 400";;
+		19) echo "Kamai 400";;
+		20) echo "Amulet 400";;
+		21) echo "Amulet 400";;
+		22) echo "Amulet 400";;
+		23) echo "Amulet 400";;
+		24) echo "Magi 450";;
+		25) echo "Kamai 400v2";;
+		26) echo "Kamai 500";;
+		27) echo "Amulet 500";;
+		28) echo "Magi 450a";;
+		29) echo "Magi 400";;
+		30) echo "Magi 410";;
+		31) echo "Kamai 450";;
+		32) echo "Amulet 450m";;
+		33) echo "Kamai X";;
+		34) echo "Aria 500c";;
+		35) echo "Aria 500i";;
+		36) echo "Aria 500t";;
+		37) echo "XTV125H-C";;
+		38) echo "XTV125H-2C";;
+		39) echo "Magi 550";;
+		40) echo "Aria 500i";;
+		41) echo "Amulet 600";;
+		42) echo "Kamai 600";;
+		*)  echo "Entone STB";;
+	esac
+}
+
+toKilo() { local n; read n; expr $n \/ 1024; }
+toMega() { local n; read n; expr $n \/ 1048576; }
+roundoff() { local x ; read x ; local n="`expr $1 / 2`" ; expr \( "$x" + "$n" \) \/ "$1" \* "$1"; }
+
+sw_version() { sed -ne "/$1/ s|.*: ||p" /tmp/diag/general/status; }
+
+get_uptime() { uptime | sed -e 's|.*up[ \t]*\([^,]\+\),.*|\1|'; }
+
+_df() { df -m | grep $1 | tail -n1 | awk "{print \$$2}"; }
+_df_unit() { local size="$(_df $*)"; if [ "$size" -gt 1024 ]; then echo "$(echo ${size} | toKilo)GB"; elif [ -n "$size" ]; then echo "${size}MB"; else echo "--"; fi ; }
+disk_size() { _df_unit $1 2; }
+disk_used() { _df_unit $1 3; }
+
+nand_size() { grep "nand0." /proc/mtd | awk '{printf "0x"$2 "\n"}' | xargs printf "%d\n" | awk '{s+=$1} END {print s / 1048576}' | roundoff 64; }
+nand_used() { expr $(nand_size) - $(_df `partition_info approotfs` 4) - $(_df /mnt/persist 4); }
+
+ram_system_total() { grep "MemTotal:" /proc/meminfo | awk '{printf $2 }'; }
+ram_system_free() { grep "MemFree:" /proc/meminfo | awk '{printf $2 }'; }
+ram_nexus_total() { heapDump 2>&1 | sed 's,.*size \([0-9]*\).*,\1,g' | awk '{s+=$1} END {print s}' | toKilo; }
+ram_nexus_free() { heapDump 2>&1 | sed 's,.*free \([0-9]*\).*,\1,g' | awk '{s+=$1} END {print s}' | toKilo; }
+ram_size() { grep "System RAM" /proc/iomem | sed 's,\([0-9a-fA-F]\{8\}\)-\([0-9a-fA-F]\{8\}\).*,-0x\1 0x\2 1,g' | xargs printf "%d\n" | awk '{s+=$1} END {print s / 1024}'; }
+ram_used() { expr $(ram_size) - $(ram_system_free) - $(ram_nexus_free); }
+
+JSON_SEPARATOR=""
+_print_value() { printf "$JSON_SEPARATOR\"$1\": \"%s\"" "$2"; JSON_SEPARATOR=", "; }
+
+#netflix_esn() { netflixESN > /dev/null; NETFLIX_ESN=$(cat /tmp/netflix-esn); }
+
+print_json() {
+	printf "{"
+	_print_value 'Model Name' "$(model_name)"
+	_print_value 'Board Version' "$(hwblk_field 'Board Version')"
+	_print_value 'Serial Number' "$(hwblk_field 'HW Model')-$(hwblk_field 'ESN')"
+	_print_value 'PBL Version' "$(sw_version pbl)"
+	_print_value 'BBL Version' "$(sw_version bbl)"
+	_print_value 'APP Version' "$(sw_version app)"
+#	netflix_esn
+#	if [ ! -z ${NETFLIX_ESN} ]; then
+#	    _print_value 'Netflix ESN' "${NETFLIX_ESN}"
+#	fi
+	_print_value 'System Uptime' "$(get_uptime)"
+	_print_value 'Flash Size (Used)' "$(nand_size)MB ($(nand_used)MB)"
+	_print_value 'Ram Size (Used)' "$(ram_size | toKilo)MB ($(ram_used | toKilo)MB)"
+	_print_value 'System Memory' "$(ram_system_free | toKilo) / $(ram_system_total | toKilo)"
+	_print_value 'Nexus Memory' "$(ram_nexus_free | toKilo) / $(ram_nexus_total | toKilo)"
+	_print_value 'persist Size (Used)' "$(disk_size /mnt/hdd) ($(disk_used /mnt/hdd))"
+	_print_value 'IP Address' "$(hwblk_field 'IP')"
+
+	# TODO: implement error code field
+	#_print_value 'Error Code' '--'
+	printf "}"
 }
 
 
